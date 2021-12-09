f35660f346cfc62d30ee9798b48e9ee6555f6ee8
acallahan97 <callahanamanda877@gmail.com>
2021-12-09 08:13:37 -0700

    initial commit

---
+++ StatementsLoopsExercise1/Program.cs
@@ -0,0 +1,72 @@
+﻿//using System;
+//
+//namespace StatementsLoopsExercise1
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello World!");
+//            for (int i = 0; i < 5; i++)
+//            {
+//                Console.WriteLine($"{i}");
+//            }
+//            for (int i = 0; i < 5; i++)
+//            {
+//                Console.WriteLine($"{i}");
+//            }
+//            for (int i = 0; i < 5; i++)
+//            {
+//                Console.WriteLine($"{i}");
+//            }
+//            for (int i = 0; i < 5; i++)
+//            {
+//                Console.WriteLine($"{i}");
+//            }
+//            for (int i = 0; i < 5; i++)
+//            {
+//                Console.WriteLine($"{i}");
+//            }
+//            for (int i = 0; i < 5; i++)
+//            {
+//                Console.WriteLine($"{i}");
+//            }
+//            for (int i = 0; i < 5; i++)
+//            {
+//                Console.WriteLine($"{i}");
+//            }
+//            for (int i = 0; i < 5; i++)
+//            {
+//                Console.WriteLine($"{i}");
+//            }
+//            for (int i = 0; i < 5; i++)
+//            {
+//                Console.WriteLine($"{i}");
+//            }
+//            for (int i = 0; i < 5; i++)
+//            {
+//                Console.WriteLine($"{i}");
+//            }
+//            for (int i = 0; i < 5; i++)
+//            {
+//                Console.WriteLine($"{i}");
+//            }
+//            for (int i = 0; i < 5; i++)
+//            {
+//                Console.WriteLine($"{i}");
+//            }
+//            for (int i = 0; i < 5; i++)
+//            {
+//                Console.WriteLine($"{i}");
+//            }
+//            for (int i = 0; i < 5; i++)
+//            {
+//                Console.WriteLine($"{i}");
+//            }
+//            for (int i = 0; i < 5; i++)
+//            {
+//                Console.WriteLine($"{i}");
+//            }
+//       }   
+//    }
+//}
